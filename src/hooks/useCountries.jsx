51067b5c93681cf96598068e18266abc09ca3003
Jenny Haddad <jennyhaddad@MacBook-Air-som-tillhor-Jenny.local>
2025-06-06 23:06:13 +0200

    testar igen

--- src/hooks/useCountries.jsx
+++ src/hooks/useCountries.jsx
@@ -7,22 +7,17 @@ const useCountries = () => {
   const [error, setError] = useState(null);
 
   useEffect(() => {
-    const url = "/.netlify/functions/getCountries";
+    const url = "https://restcountries.com/v3.1/all";
 
     const fetchCountries = async () => {
       try {
         const response = await fetch(url);
-
-        console.log("Response object:", response);
 
         if (!response.ok) {
           throw new Error(`HTTP-fel! Status: ${response.status}`);
         }
 
         const data = await response.json();
-
-        console.log("Fetching countries from:", url);
-
         setCountries(data);
       } catch (err) {
         console.error("API Error:", err);
